4aa643e3b5205377bedf2a1782bc46b221b4c65a
Ricardo <ricardoledan@proton.me>
2025-07-28 22:31:40 -0500

    updates to homemanager

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -18,7 +18,7 @@
     };
     
     # Initialize powerlevel10k
-    initExtra = ''
+    initContent = ''
       # Enable Powerlevel10k instant prompt
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
